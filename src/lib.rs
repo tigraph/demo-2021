15973d952f40aa23b53dc8d9fb8affc752f969e2
hi-rustin <rustin.liu@gmail.com>
2022-01-09 16:23:15 +0800

    Update args
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-const MIN_FRIENDS_COUNT: i64 = 10;
-const MAX_FRIENDS_COUNT: i64 = 30;
+const MIN_FRIENDS_COUNT: i64 = 30;
+const MAX_FRIENDS_COUNT: i64 = 50;
 
 pub mod alter_data_generator;
 pub mod n_degree_data_generator;
